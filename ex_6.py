9283d2c298f699c1235a4bdff2db8332f210d2c6
caiwenbin <346904878@qq.com>
2019-08-22 19:11:18 +0800

    读写文件

--- ex_6.py
+++ ex_6.py
@@ -7,3 +7,27 @@ print("if you don't want that, hit CTRL+C (^C).")
 print("if you do want that, hit RETURN.")
 
 input("?")
+
+print("opening the file...")
+target = open(filename, 'w')
+
+print("truncating the file. goodbye!")
+target.truncate()
+
+print("now i'm going to ask you for three lines.")
+
+line1 = input("line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print("i'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("and finally, we close it.")
+target.close()
