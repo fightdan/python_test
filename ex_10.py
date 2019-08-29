1a18e8ecd7df31ac0c4c5799af1a6bbc102c2a16
caiwenbin <346904878@qq.com>
2019-08-29 16:55:37 +0800

    函数返回值

--- ex_10.py
+++ ex_10.py
@@ -3,17 +3,17 @@ from sys import argv
 script, input_file = argv
 
 def print_all(f):
-    print(f.read())
+    print(f.read(), end='')
 
 def rewind(f):
     f.seek(0)
 
 def print_a_line(line_count, f):
-    print(line_count, f.readline())
+    print(line_count, f.readline(), end='')
     
 current_file = open(input_file)
 
-print("first let's print the whole file:\n")
+print("first let's print the whole file:")
 
 print_all(current_file)
 
